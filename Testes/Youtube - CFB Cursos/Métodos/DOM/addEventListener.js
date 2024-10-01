51113b6406f26e960270ab55ad7920b7370b8706
MauricioL88 <lisboa.sse@gmail.com>
2024-10-01 17:09:43 -0300

    estudando addEventlistener

---
+++ Testes/Youtube - CFB Cursos/Métodos/DOM/addEventListener.js
@@ -0,0 +1,7 @@
+// Capturar as propriedades da tag para uma constante
+const b1 = document.getElementById("b1");
+
+// Atribuindo um evento diretamente na constante
+b1.addEventListener("click",() => {
+    alert("Resultado do Clique!");
+});
